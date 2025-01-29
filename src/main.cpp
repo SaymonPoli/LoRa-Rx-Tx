1598b0955deffbce6d1467bd95e7c51303b4fb43
saymon.poli <saymon.mar.pol@gmail.com>
2025-01-29 19:59:14 -0300

    better comments

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,21 @@ ICACHE_RAM_ATTR
 #if defined(ESP32)
 ICACHE_RAM_ATTR
 #endif
+
+/*
+  Import Tx if the device is Tx
+*/
 #ifdef TX_DEVICE
+/*
+  Import Tx class and initialize the radio
+*/
 #include "../src/header/Tx.h"
 TxRadio *radio = new TxRadio;
 #endif
 
+/*
+  Import if the device is Rx
+*/
 #ifdef RX_DEVICE
 #include "../src/header/Rx.h"
 RxRadio *radio = new RxRadio;
